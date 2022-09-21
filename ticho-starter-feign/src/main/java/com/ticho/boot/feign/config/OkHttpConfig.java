d41bb21bc4152e90f32b60011eacc155392ad44b
zhajianjun <1019319473@qq.com>
2022-09-21 18:26:59 +0800

    feign 添加配置、添加security-jwt pom

--- ticho-starter-feign/src/main/java/com/ticho/boot/feign/config/OkHttpConfig.java
+++ ticho-starter-feign/src/main/java/com/ticho/boot/feign/config/OkHttpConfig.java
@@ -1,9 +1,17 @@
 package com.ticho.boot.feign.config;
 
+import feign.Logger;
+import okhttp3.ConnectionPool;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * RestTemplate配置
@@ -17,7 +25,35 @@ public class OkHttpConfig {
      * 基于OkHttp3配置RestTemplate
      */
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    public RestTemplate restTemplate(OkHttp3ClientHttpRequestFactory httpRequestFactory) {
+        return new RestTemplate(httpRequestFactory);
     }
+
+    @Bean
+    public Logger.Level log() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        // @formatter:off
+        return new OkHttpClient().newBuilder()
+            //设置连接超时
+            .connectTimeout(10 , TimeUnit.SECONDS)
+            //设置读超时
+            .readTimeout(10 , TimeUnit.SECONDS)
+            //设置写超时
+            .writeTimeout(10 , TimeUnit.SECONDS)
+            //是否自动重连
+            .retryOnConnectionFailure(true)
+            .connectionPool(new ConnectionPool(10 , 5L, TimeUnit.MINUTES))
+            .build();
+        // @formatter:on
+    }
+
+    @Bean
+    public OkHttp3ClientHttpRequestFactory httpRequestFactory(OkHttpClient okHttpClient) {
+        return new OkHttp3ClientHttpRequestFactory(okHttpClient);
+    }
+
 }
