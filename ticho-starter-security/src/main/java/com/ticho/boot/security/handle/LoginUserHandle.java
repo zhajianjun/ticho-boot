bded817609018ec6675a767ced93e5e31a023565
zhajianjun <1019319473@qq.com>
2022-11-09 11:09:50 +0800

    代码优化

--- ticho-starter-security/src/main/java/com/ticho/boot/security/handle/LoginUserHandle.java
+++ ticho-starter-security/src/main/java/com/ticho/boot/security/handle/LoginUserHandle.java
@@ -2,7 +2,6 @@ package com.ticho.boot.security.handle;
 
 import com.ticho.boot.security.dto.LoginRequest;
 import com.ticho.boot.security.dto.Oauth2AccessToken;
-import org.springframework.stereotype.Component;
 
 /**
  * 登录处理接口
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Component;
  * @author zhajianjun
  * @date 2022-09-22 18:12
  */
-@Component
 public interface LoginUserHandle {
 
     /**
