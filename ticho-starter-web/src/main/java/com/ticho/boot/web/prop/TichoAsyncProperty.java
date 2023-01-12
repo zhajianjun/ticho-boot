f83ba2966a27d00559cc27399609ff75a9878eec
zhajianjun <1019319473@qq.com>
2023-01-12 16:37:34 +0800

    日志由tlog改为sleuth

---
+++ ticho-starter-web/src/main/java/com/ticho/boot/web/prop/TichoAsyncProperty.java
@@ -0,0 +1,40 @@
+package com.ticho.boot.web.prop;
+
+import lombok.Data;
+
+/**
+ *
+ *
+ * @author zhajianjun
+ * @date 2023-01-12 11:08
+ */
+@Data
+public class TichoAsyncProperty {
+
+    /**
+     * 开启异步线程池
+     */
+    private boolean enableAsync = true;
+
+    /**
+     * 核心线程数，线程池维护线程的最小数量.
+     */
+    private int corePoolSize = Runtime.getRuntime().availableProcessors();
+    /**
+     * 最大线程数，线程池维护线程的最大数量
+     */
+    private int maxPoolSize = corePoolSize * 2;
+    /**
+     * 队列容量
+     */
+    private int queueCapacity = 20;
+    /**
+     * 线程活跃时间（秒）
+     */
+    private int keepAliveSeconds = 60;
+    /**
+     * 设置线程名称前缀
+     */
+    private String threadNamePrefix = "ticho";
+
+}
