8328878f5c2d8b6b44017ed526ff2eff57e2da16
zhajianjun <1019319473@qq.com>
2023-02-03 17:35:27 +0800

    redis配置优化

--- ticho-starter-redis/src/main/java/com/ticho/boot/redis/config/TichoRedisConfig.java
+++ ticho-starter-redis/src/main/java/com/ticho/boot/redis/config/TichoRedisConfig.java
@@ -4,13 +4,11 @@ import com.ticho.boot.redis.util.RedisUtil;
 import org.redisson.Redisson;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
@@ -26,14 +24,6 @@ public class TichoRedisConfig {
 
     @Bean
     @ConditionalOnMissingBean(RedisUtil.class)
-    @ConditionalOnBean(RedisConnectionFactory.class)
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        return new StringRedisTemplate(redisConnectionFactory);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(RedisUtil.class)
-    @ConditionalOnBean(StringRedisTemplate.class)
     public RedisUtil<String, String> redisUtil(StringRedisTemplate stringRedisTemplate) {
         return new RedisUtil<>(stringRedisTemplate);
     }
