ff961d599ad4d04cb34adaf15466879401bf0669
zhajianjun <1019319473@qq.com>
2023-02-08 11:15:19 +0800

    添加gateway-starter

---
+++ ticho-starter-gateway/src/main/java/com/ticho/boot/gateway/filter/SecurityHeaderFilter.java
@@ -0,0 +1,28 @@
+package com.ticho.boot.gateway.filter;
+
+import com.ticho.boot.gateway.constant.CommonConst;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * 权限请求头处理
+ *
+ * @author zhajianjun
+ * @date 2021-10-18 22:57
+ */
+@Component
+public class SecurityHeaderFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpRequest newRequest = request.mutate().header(CommonConst.INNER, CommonConst.INNER_VALUE).build();
+            ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
+            return chain.filter(newExchange);
+        };
+    }
+}
