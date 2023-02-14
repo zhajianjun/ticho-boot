c1548d4d366bc24362779108c34d09e86d1d8044
zhajianjun <1019319473@qq.com>
2023-02-14 15:02:23 +0800

    用户登录查询优化

--- ticho-starter-security/src/main/java/com/ticho/boot/security/dto/LoginRequest.java
+++ ticho-starter-security/src/main/java/com/ticho/boot/security/dto/LoginRequest.java
@@ -1,29 +1,32 @@
 package com.ticho.boot.security.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-
 /**
- * 登录请求参数
+ * 登录请求参数接口
  *
  * @author zhajianjun
  * @date 2022-09-22 16:35
  */
-@Data
-@ApiModel("登录请求参数")
-public class LoginRequest {
+public interface LoginRequest {
 
     /** 用户名 */
-    @ApiModelProperty(value = "用户名", position = 10)
-    private String username;
+    String getUsername();
+
+    void setUsername(String username) ;
 
     /** 密码 */
-    @ApiModelProperty(value = "密码", position = 20)
-    private String password;
+    String getPassword() ;
+
+    void setPassword(String password) ;
 
     /** 登录类型 */
-    @ApiModelProperty(value = "登录类型", position = 30)
-    private String type;
+    String getType();
+
+    void setType(String type) ;
+
+
+
+
+
+
 
 }
