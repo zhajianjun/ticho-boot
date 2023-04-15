1ab692add7957434b2c8619ca27110b54d2546e8
zhajianjun <1019319473@qq.com>
2023-04-15 13:08:47 +0800

    添加ticho-starter-es

---
+++ ticho-starter-es/src/main/java/com/ticho/boot/es/entity/Entity.java
@@ -0,0 +1,15 @@
+package com.ticho.boot.es.entity;
+
+/**
+ *
+ *
+ * @author zhajianjun
+ * @date 2023-04-15 12:17:55
+ */
+public interface Entity {
+
+    String getId();
+
+    void setId(String id);
+
+}
