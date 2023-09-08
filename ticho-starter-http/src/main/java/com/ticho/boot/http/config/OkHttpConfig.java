55f9b0cb4d3cfbadcb96da82b861e3df85e97f96
zhajianjun <1019319473@qq.com>
2023-09-08 10:24:10 +0800

    feign底层使用okhttp，并且支持忽略校验ssl证书

--- ticho-starter-http/src/main/java/com/ticho/boot/http/config/OkHttpConfig.java
+++ ticho-starter-http/src/main/java/com/ticho/boot/http/config/OkHttpConfig.java
@@ -16,6 +16,14 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -69,6 +77,10 @@ public class OkHttpConfig {
             // 是否自动重连
             .retryOnConnectionFailure(prop.getRetryOnConnectionFailure())
             .connectionPool(new ConnectionPool(prop.getMaxIdleConnections() , prop.getKeepAliveDuration(), TimeUnit.MINUTES))
+            // 信任所有证书
+            .sslSocketFactory(createSSLSocketFactory(), new TrustAllCerts())
+            // 主机名验证，信任所有
+            .hostnameVerifier(new TrustAllHostnameVerifier())
             .build();
         // @formatter:on
     }
@@ -88,6 +100,40 @@ public class OkHttpConfig {
     @ConditionalOnMissingBean({RestTemplate.class})
     public RestTemplate restTemplate(OkHttp3ClientHttpRequestFactory httpRequestFactory) {
         return new RestTemplate(httpRequestFactory);
+    }
+
+
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception ignored) {
+        }
+        return ssfFactory;
+    }
+
+    private static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
     }
 
 }
