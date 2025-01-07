15185d00b59cbaf535c2431dea04a8c45ffdb1a2
zhajianjun <1019319473@qq.com>
2025-01-07 23:52:30 +0800

    feat[ticho-starter-datasource]: 增强RootMapper接口文档和参数注释
    Signed-off-by: zhajianjun <1019319473@qq.com>

--- ticho-starter-datasource/src/main/java/top/ticho/boot/datasource/mapper/RootMapper.java
+++ ticho-starter-datasource/src/main/java/top/ticho/boot/datasource/mapper/RootMapper.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Param;
 import java.util.Collection;
 
 /**
+ * RootMapper接口，继承自MyBatis-Plus的BaseMapper，提供自定义的批量操作方法
+ *
  * @author zhajianjun
  * @date 2022-10-17 08:53
  */
@@ -13,25 +15,33 @@ public interface RootMapper<T> extends BaseMapper<T> {
 
     /**
      * 自定义批量插入
-     * 如果要自动填充，@Param(xx) xx参数名必须是 list/collection/array 3个的其中之一
+     *
+     * @param list 要插入的实体对象集合
+     * @return 插入操作影响的行数
      */
     int insertBatch(@Param("list") Collection<T> list);
 
     /**
-     * 自定义批量更新，条件为主键
-     * 如果要自动填充，@Param(xx) xx参数名必须是 list/collection/array 3个的其中之一
+     * 自定义批量更新
+     *
+     * @param list 要更新的实体对象集合
+     * @return 更新操作影响的行数
      */
     int updateBatch(@Param("list") Collection<T> list);
 
     /**
-     * 自定义新增或者更新，条件为主键
-     * 如果要自动填充，@Param(xx) xx参数名必须是 list/collection/array 3个的其中之一
+     * 自定义新增或者更新
+     *
+     * @param data 要插入或更新的实体对象
+     * @return 插入或更新操作影响的行数
      */
     int insertOrUpdate(T data);
 
     /**
-     * 自定义批量新增或者更新，条件为主键
-     * 如果要自动填充，@Param(xx) xx参数名必须是 list/collection/array 3个的其中之一
+     * 自定义批量新增或者更新
+     *
+     * @param list 要插入或更新的实体对象集合
+     * @return 插入或更新操作影响的行数
      */
     int insertOrUpdateBatch(@Param("list") Collection<T> list);
 
