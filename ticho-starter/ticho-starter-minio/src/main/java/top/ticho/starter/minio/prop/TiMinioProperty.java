163702e5651e922a07d61a6af80513c31e654f71
zhajianjun <1019319473@qq.com>
2025-02-02 22:01:03 +0800

    refactor[ticho-starter-minio]: 优化 MinIO 配置属性
    [移除了 TiMinioProperty 类中的多余空行- 保持了代码的整洁和一致性]

--- ticho-starter/ticho-starter-minio/src/main/java/top/ticho/starter/minio/prop/TiMinioProperty.java
+++ ticho-starter/ticho-starter-minio/src/main/java/top/ticho/starter/minio/prop/TiMinioProperty.java
@@ -14,28 +14,20 @@ public class TiMinioProperty {
 
     /** 是否开启配置 */
     private Boolean enable;
-
     /** minio的地址 */
     private String endpoint;
-
     /** 用户名 */
     private String accessKey;
-
     /** 密码 */
     private String secretKey;
-
     /** 图片大小限制，默认20MB */
     private DataSize maxImgSize = DataSize.ofMegabytes(20L);
-
     /** 文件大小限制，默认20MB */
     private DataSize maxFileSize = DataSize.ofMegabytes(20L);
-
     /** 分段上传大小，最小5MB,最大5GB */
     private Long partSize = 5 * 1024 * 1024L;
-
     /** 默认桶 */
     private String defaultBucket = "default";
-
     /** 分片桶 */
     private String chunkBucket = "chunk";
 
