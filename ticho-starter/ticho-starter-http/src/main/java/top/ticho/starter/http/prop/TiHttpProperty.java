c908e9b773b6352c9f32152fe9c1f276acfdc633
zhajianjun <1019319473@qq.com>
2025-02-08 20:52:21 +0800

    refactor[ticho-starter-http]: 移除 TiHttpProperty 类中的冗余注释
    [移除了 TiHttpProperty 类中不必要的空行和多余的注释符号，包括：
    - 删除了类属性之间的多余空行
    - 统一了注释的格式，去除了多余的连字符
    - 调整了部分注释的标点符号，使其更加规范
    
    这些修改提高了代码的可读性和一致性，便于其他开发人员理解和维护。]
    
    Signed-off-by: zhajianjun <1019319473@qq.com>

--- ticho-starter/ticho-starter-http/src/main/java/top/ticho/starter/http/prop/TiHttpProperty.java
+++ ticho-starter/ticho-starter-http/src/main/java/top/ticho/starter/http/prop/TiHttpProperty.java
@@ -13,28 +13,20 @@ public class TiHttpProperty {
 
     /** 开启ticho http，默认使用okhttp */
     private Boolean enable = true;
-
     /** 开启日志拦截 */
     private Boolean openLog = true;
-
     /* 日志拦截是否打印日志 */
     private Boolean printLog = false;
-
     /** 设置连接超时，单位:秒(s) */
     private Long connectTimeout = 10L;
-
     /** 设置读超时，单位:秒(s) */
     private Long readTimeout = 10L;
-
     /** 设置写超时，单位:秒(s) */
     private Long writeTimeout = 10L;
-
     /** 是否自动重连 */
     private Boolean retryOnConnectionFailure = true;
-
     /** 最大连接数 */
     private int maxIdleConnections = 10;
-
     /** 存活时间，单位:分钟(min) */
     private Long keepAliveDuration = 5L;
 
