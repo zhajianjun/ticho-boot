ba1f97c4d692dbf3d5a8b153252f5bccb6d0e787
zhajianjun <1019319473@qq.com>
2025-02-12 22:41:35 +0800

    refactor[ticho-starter-redis]: 重命名 Redis 配置属性类
    Signed-off-by: zhajianjun <1019319473@qq.com>

--- ticho-starter/ticho-starter-redis/src/main/java/top/ticho/starter/redis/prop/TiRedisProperty.java
+++ ticho-starter/ticho-starter-redis/src/main/java/top/ticho/starter/redis/prop/TiRedisProperty.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "ticho.redis")
-public class TiBaseRedisProperty {
+public class TiRedisProperty {
 
     private String host;
 
