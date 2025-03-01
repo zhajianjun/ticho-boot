8f94d1ae6b9bc3ee75bf2a4ce91c070e49f2a803
adaratutto <1401007725@qq.com>
2025-03-01 20:58:49 +0800

    refactor[ticho-starter-datasource]: 重构分页工具类
    [- 修改方法名：将 getResult 方法统一命名为 of，提高代码一致性
    - 增加泛型转换功能：新增带有 Function 参数的 of 方法，支持页面数据的自定义转换
    - 优化代码结构：引入 java.util.function 和 java.util.stream 包，使用函数式编程简化代码
    - 提升可读性和扩展性：通过参数化类型和函数映射，使 TiPageUtil 类更加灵活和易于扩展]

--- ticho-starter/ticho-starter-datasource/src/main/java/top/ticho/starter/datasource/util/TiPageUtil.java
+++ ticho-starter/ticho-starter-datasource/src/main/java/top/ticho/starter/datasource/util/TiPageUtil.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import top.ticho.starter.view.core.TiPageResult;
 
 import java.math.BigDecimal;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 分页对象转换工具
@@ -16,7 +18,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TiPageUtil {
 
-    public static <T> TiPageResult<T> getResult(Page<T> page) {
+    public static <T> TiPageResult<T> of(Page<T> page) {
         TiPageResult<T> tiPageResult = new TiPageResult<>();
         tiPageResult.setPageNum(page.getPageNum());
         tiPageResult.setPageSize(page.getPageSize());
@@ -26,13 +28,33 @@ public class TiPageUtil {
         return tiPageResult;
     }
 
-    public static <T> TiPageResult<T> getResult(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page) {
+    public static <T, R> TiPageResult<R> of(Page<T> page, Function<T, R> function) {
+        TiPageResult<R> tiPageResult = new TiPageResult<>();
+        tiPageResult.setPageNum(page.getPageNum());
+        tiPageResult.setPageSize(page.getPageSize());
+        tiPageResult.setPages(page.getPages());
+        tiPageResult.setTotal(BigDecimal.valueOf(page.getTotal()).intValue());
+        tiPageResult.setRows(page.getResult().stream().map(function).collect(Collectors.toList()));
+        return tiPageResult;
+    }
+
+    public static <T> TiPageResult<T> of(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page) {
         TiPageResult<T> tiPageResult = new TiPageResult<>();
         tiPageResult.setPageNum(Long.valueOf(page.getCurrent()).intValue());
         tiPageResult.setPageSize(Long.valueOf(page.getSize()).intValue());
         tiPageResult.setPages(Long.valueOf(page.getPages()).intValue());
         tiPageResult.setTotal(BigDecimal.valueOf(page.getTotal()).intValue());
         tiPageResult.setRows(page.getRecords());
+        return tiPageResult;
+    }
+
+    public static <T, R> TiPageResult<R> of(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> page, Function<T, R> function) {
+        TiPageResult<R> tiPageResult = new TiPageResult<>();
+        tiPageResult.setPageNum(Long.valueOf(page.getCurrent()).intValue());
+        tiPageResult.setPageSize(Long.valueOf(page.getSize()).intValue());
+        tiPageResult.setPages(Long.valueOf(page.getPages()).intValue());
+        tiPageResult.setTotal(BigDecimal.valueOf(page.getTotal()).intValue());
+        tiPageResult.setRows(page.getRecords().stream().map(function).collect(Collectors.toList()));
         return tiPageResult;
     }
 
