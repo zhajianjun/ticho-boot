5e10d5d44935cfcc29d09317412064b6c1655e5a
zhajianjun <1019319473@qq.com>
2025-03-09 19:34:48 +0800

    refactor[ticho-starter-security]: 移除未使用的用户类型常量
    [- 删除了 TiSecurityConst 类中与用户类型和登录方式相关的多个常量
    - 优化了代码结构，减少了冗余定义]
    
    Signed-off-by: zhajianjun <1019319473@qq.com>

--- ticho-starter/ticho-starter-security/src/main/java/top/ticho/starter/security/constant/TiSecurityConst.java
+++ ticho-starter/ticho-starter-security/src/main/java/top/ticho/starter/security/constant/TiSecurityConst.java
@@ -35,35 +35,8 @@ public class TiSecurityConst {
 
     public static final String PM = "pm";
 
-
-    /** 加载用户类型: */
-    public static final String LOAD_USER_TYPE = "load_user_type:";
-
-    /** 登录方式: */
-    public static final String LOGIN_USER_TYPE = "login_user_type:";
-
     /** 用户密码 */
     public static final String USERNAME = "username";
-    /** 手机号码 */
-    public static final String EMAIL = "email";
-    /** 邮箱 */
-    public static final String PHONE = "phone";
-
-    /** 加载用户类型:用户密码 */
-    public static final String LOAD_USER_TYPE_USERNAME = LOAD_USER_TYPE + USERNAME;
-    /** 加载用户类型:手机号码 */
-    public static final String LOAD_USER_TYPE_PHONE = LOAD_USER_TYPE + EMAIL;
-    /** 加载用户类型:邮箱 */
-    public static final String LOAD_USER_TYPE_EMAIL = LOAD_USER_TYPE + PHONE;
-
-
-    /** 登录方式:用户密码 */
-    public static final String LOGIN_USER_TYPE_USERNAME = LOGIN_USER_TYPE + USERNAME;
-    /** 登录方式:手机号码 */
-    public static final String LOGIN_USER_TYPE_PHONE = LOGIN_USER_TYPE + EMAIL;
-    /** 登录方式:邮箱 */
-    public static final String LOGIN_USER_TYPE_EMAIL = LOGIN_USER_TYPE + PHONE;
-
 
     public static final String OAUTH2_TOKEN_FILTER_BEAN_NAME = "oauth2_token_filter_bean_name";
 
