8223190b3186e57ecd61fb8f7f41e4e9505872d7
zhajianjun <1019319473@qq.com>
2025-04-18 00:03:26 +0800

    feat[ticho-starter-security]: 添加@EnableMethodSecurity注解，开启方法级@PreAuthorize安全控制

--- ticho-starter/ticho-starter-security/src/main/java/top/ticho/starter/security/config/TiWebSecurityConfig.java
+++ ticho-starter/ticho-starter-security/src/main/java/top/ticho/starter/security/config/TiWebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,6 +31,7 @@ import top.ticho.starter.security.filter.TiAuthorizationManager;
  */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class TiWebSecurityConfig {
 
     @Resource
