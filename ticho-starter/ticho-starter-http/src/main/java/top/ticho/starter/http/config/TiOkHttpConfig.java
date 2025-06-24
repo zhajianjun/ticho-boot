eb03d8fa8def020472d2d3b5936d3e2b35ff613d
zhajianjun <1019319473@qq.com>
2025-06-24 21:35:31 +0800

    fix[ticho-starter-http]: 移除无用的引包

--- ticho-starter/ticho-starter-http/src/main/java/top/ticho/starter/http/config/TiOkHttpConfig.java
+++ ticho-starter/ticho-starter-http/src/main/java/top/ticho/starter/http/config/TiOkHttpConfig.java
@@ -1,9 +1,6 @@
 package top.ticho.starter.http.config;
 
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
