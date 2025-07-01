eabfaa32f8738504733d06d8bbf66f28996e5c76
zhajianjun <1019319473@qq.com>
2025-07-01 22:59:27 +0800

    refactor[ticho-trace]: 移除 TiTraceProperty 中的冗余字段

--- ticho-trace/ticho-trace-common/src/main/java/top/ticho/trace/common/prop/TiTraceProperty.java
+++ ticho-trace/ticho-trace-common/src/main/java/top/ticho/trace/common/prop/TiTraceProperty.java
@@ -17,18 +17,8 @@ public class TiTraceProperty {
 
     /** 是否开启链路拦截器 */
     private Boolean enable = true;
-    /** 链路服务url */
-    private String url;
-    /** 日志收集服务url */
-    private String logUrl;
-    /** 链路服务秘钥 */
-    private String secret;
     /** 链路表达式 */
     private String trace = TiTraceConst.DEFAULT_TRACE;
-    /** 是否推送日志 */
-    private Boolean pushLog = false;
-    /** 是否推送链路信息 */
-    private Boolean pushTrace = false;
     /** 链路过滤地址 */
     private List<String> antPatterns = new ArrayList<>();
     /** 拦截器排序 */
