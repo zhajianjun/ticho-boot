17f1e790ca8c51b82e7ed0314fca5026e29d0770
zhajianjun <1019319473@qq.com>
2025-08-20 21:15:04 +0800

    feat[ticho-tool-template]: 增加对文件模板的支持并优化模板渲染功能
    [- 新增 gtForFile 和 gtForString 两个 GroupTemplate 实例，分别用于处理文件模板和字符串模板
    - 添加 render 方法的重载版本，支持传入单个参数和是否为classpath的标志
    - 优化现有的 render 方法，增加参数空值判断和处理
    - 引入 ticho-tool-core 依赖，以使用 TiMapUtil、TiObjUtil 和 TiStrUtil 工具类]

--- ticho-tool/ticho-tool-template/src/main/java/top/ticho/tool/template/TiTemplateUtil.java
+++ ticho-tool/ticho-tool-template/src/main/java/top/ticho/tool/template/TiTemplateUtil.java
@@ -4,7 +4,11 @@ import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
 import org.beetl.core.ReThrowConsoleErrorHandler;
 import org.beetl.core.Template;
+import org.beetl.core.resource.ClasspathResourceLoader;
 import org.beetl.core.resource.StringTemplateResourceLoader;
+import top.ticho.tool.core.TiMapUtil;
+import top.ticho.tool.core.TiObjUtil;
+import top.ticho.tool.core.TiStrUtil;
 
 import java.io.IOException;
 import java.util.Map;
@@ -14,20 +18,38 @@ import java.util.Map;
  * @date 2025-08-04 22:33
  */
 public class TiTemplateUtil {
-    private static GroupTemplate gt;
+    private static final GroupTemplate gtForString;
+    private static final GroupTemplate gtForFile;
 
     static {
-        gt = null;
-        StringTemplateResourceLoader resourceLoader = new StringTemplateResourceLoader();
+        StringTemplateResourceLoader stringTemplateResourceLoader = new StringTemplateResourceLoader();
+        ClasspathResourceLoader classpathResourceLoader = new ClasspathResourceLoader();
         Configuration cfg;
         try {
             cfg = Configuration.defaultConfiguration();
         } catch (IOException var3) {
             throw new RuntimeException(var3);
         }
+        gtForString = new GroupTemplate(stringTemplateResourceLoader, cfg);
+        gtForString.setErrorHandler(new ReThrowConsoleErrorHandler());
+        gtForFile = new GroupTemplate(classpathResourceLoader, cfg);
+        gtForFile.setErrorHandler(new ReThrowConsoleErrorHandler());
+    }
 
-        gt = new GroupTemplate(resourceLoader, cfg);
-        gt.setErrorHandler(new ReThrowConsoleErrorHandler());
+    public static String render(String template, Map<String, ?> paramsMap) {
+        Template t = gtForString.getTemplate(template);
+        if (TiMapUtil.isNotEmpty(paramsMap)) {
+            t.binding(paramsMap);
+        }
+        return t.render();
+    }
+
+    public static String render(String template, String key, Object value) {
+        Template t = gtForString.getTemplate(template);
+        if (TiStrUtil.isNotBlank(key)) {
+            t.binding(key, TiObjUtil.defaultIfNull(value, TiStrUtil.EMPTY));
+        }
+        return t.render();
     }
 
     /**
@@ -37,9 +59,19 @@ public class TiTemplateUtil {
      * @param paramsMap 参数map
      * @return {@link String}
      */
-    public static String render(String template, Map<String, ?> paramsMap) {
-        Template t = gt.getTemplate(template);
-        t.binding(paramsMap);
+    public static String render(String template, Map<String, ?> paramsMap, boolean isClasspath) {
+        Template t = isClasspath ? gtForFile.getTemplate(template) : gtForString.getTemplate(template);
+        if (TiMapUtil.isNotEmpty(paramsMap)) {
+            t.binding(paramsMap);
+        }
+        return t.render();
+    }
+
+    public static String render(String template, String key, Object value, boolean isClasspath) {
+        Template t = isClasspath ? gtForFile.getTemplate(template) : gtForString.getTemplate(template);
+        if (TiStrUtil.isNotBlank(key)) {
+            t.binding(key, TiObjUtil.defaultIfNull(value, TiStrUtil.EMPTY));
+        }
         return t.render();
     }
 
