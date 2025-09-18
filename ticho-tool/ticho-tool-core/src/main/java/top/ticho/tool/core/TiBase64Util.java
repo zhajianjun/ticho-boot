b399a642d8b1abb4c9d419a5fd5a8e4681c80638
zhajianjun <1019319473@qq.com>
2025-09-18 23:26:02 +0800

    refactor[ticho-tool-core]: 重构 Base64 加解密逻辑
    [- 移除 apache commons codec 依赖，改用 java.util.Base64
    - 优化 encode 和 decode 方法的实现
    - 统一使用 UTF-8 编码进行字符串和字节数组之间的转换]

--- ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/TiBase64Util.java
+++ ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/TiBase64Util.java
@@ -1,9 +1,8 @@
 package top.ticho.tool.core;
 
 
-import org.apache.commons.codec.binary.Base64;
-
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  *
@@ -17,21 +16,24 @@ public class TiBase64Util {
         if (source == null || source.length == 0) {
             return null;
         }
-        return Base64.encodeBase64String(source);
+        byte[] encode = Base64.getEncoder().encode(source);
+        return new String(encode, StandardCharsets.UTF_8);
     }
 
     public static String encode(String source) {
         if (TiStrUtil.isEmpty(source)) {
             return null;
         }
-        return Base64.encodeBase64String(source.getBytes(StandardCharsets.UTF_8));
+        byte[] encode = Base64.getEncoder().encode(source.getBytes(StandardCharsets.UTF_8));
+        return new String(encode, StandardCharsets.UTF_8);
     }
 
     public static String decode(String source) {
         if (TiStrUtil.isEmpty(source)) {
             return null;
         }
-        return new String(Base64.decodeBase64(source), StandardCharsets.UTF_8);
+        byte[] decode = Base64.getDecoder().decode(source.getBytes(StandardCharsets.UTF_8));
+        return new String(decode, StandardCharsets.UTF_8);
     }
 
 }
