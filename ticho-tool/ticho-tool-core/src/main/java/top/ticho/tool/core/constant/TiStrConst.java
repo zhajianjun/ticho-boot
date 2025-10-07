105fe372a1632e64e714d3eb3fe0588679c1dc7d
zhajianjun <1019319473@qq.com>
2025-10-07 17:10:05 +0800

    feat[ticho-tool-core]: 添加常用字符串常量
    [- 新增空格符、制表符、斜杠等字符串常量定义
    - 补充回车符、换行符、连接符等字符常量
    - 增加花括号、中括号等符号常量定义
    - 定义单双引号及特殊符号如艾特、冒号等常量
    - 整理并完善字符串常量注释文档
    - 移动点号常量定义位置以保持代码整洁性]

--- ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/constant/TiStrConst.java
+++ ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/constant/TiStrConst.java
@@ -9,8 +9,86 @@ package top.ticho.tool.core.constant;
 public class TiStrConst {
 
     public static final String EMPTY = "";
-    public static final String DOT = ".";
     public static final String EMPTY_JSON = "{}";
+    
+    /**
+     * 字符常量：空格符 {@code " "}
+     */
+    public static final String SPACE = " ";
+    /**
+     * 字符常量：制表符 {@code "\t"}
+     */
+    public static final String TAB = "	";
+    /**
+     * 字符常量：点 {@code "."}
+     */
+    public static final String DOT = ".";
+    /**
+     * 字符常量：斜杠 {@code "/"}
+     */
+    public static final String SLASH = "/";
+    /**
+     * 字符常量：反斜杠 {@code "\\"}
+     */
+    public static final String BACKSLASH = "\\";
+    /**
+     * 字符常量：回车符 {@code "\r"}
+     */
+    public static final String CR = "\r";
+    /**
+     * 字符常量：换行符 {@code "\n"}
+     */
+    public static final String LF = "\n";
+    /**
+     * 字符常量：减号（连接符） {@code "-"}
+     */
     public static final String DASHED = "-";
+    /**
+     * 字符常量：下划线 {@code "_"}
+     */
+    public static final String UNDERLINE = "_";
+    /**
+     * 字符常量：逗号 {@code ","}
+     */
+    public static final String COMMA = ",";
+    /**
+     * 字符常量：花括号（左） <code>"{"</code>
+     */
+    public static final String DELIM_START = "{";
+    /**
+     * 字符常量：花括号（右） <code>"}"</code>
+     */
+    public static final String DELIM_END = "}";
+    /**
+     * 字符常量：中括号（左） {@code "["}
+     */
+    public static final String BRACKET_START = "[";
+    /**
+     * 字符常量：中括号（右） {@code "]"}
+     */
+    public static final String BRACKET_END = "]";
+    /**
+     * 字符常量：双引号 {@code "'"}
+     */
+    public static final String DOUBLE_QUOTES = "'";
+    /**
+     * 字符常量：单引号 {@code "\""}
+     */
+    public static final String SINGLE_QUOTE = "\"";
+    /**
+     * 字符常量：与 {@code "&"}
+     */
+    public static final String AMP = "&";
+    /**
+     * 字符常量：冒号 {@code ":"}
+     */
+    public static final String COLON = ":";
+    /**
+     * 字符常量：艾特 {@code "@"}
+     */
+    public static final String AT = "@";
+
+    public static final String DL = "$";
+    
 
 }
