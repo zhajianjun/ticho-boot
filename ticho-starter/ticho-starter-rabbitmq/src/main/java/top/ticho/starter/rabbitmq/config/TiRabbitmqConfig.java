e54470a81dc6f03a65b5b5e4c1f74d2b1bcda202
zhajianjun <1019319473@qq.com>
2025-12-03 19:43:42 +0800

    fix[ticho-starter-rabbitmq]: 更新消息转换器类名并调整配置属性
    [- 将 Jackson2JsonMessageConverter 更改为 JacksonJsonMessageConverter
    - 修改 publisher-confirms 配置为 publisher-confirm-type 并设置默认值 none
    - 将 retry.max-attempts 配置项更正为 max-retries]

--- ticho-starter/ticho-starter-rabbitmq/src/main/java/top/ticho/starter/rabbitmq/config/TiRabbitmqConfig.java
+++ ticho-starter/ticho-starter-rabbitmq/src/main/java/top/ticho/starter/rabbitmq/config/TiRabbitmqConfig.java
@@ -5,7 +5,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.ReturnedMessage;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
@@ -38,7 +38,7 @@ public class TiRabbitmqConfig implements RabbitTemplate.ConfirmCallback, RabbitT
     public void init() {
         rabbitTemplate.setConfirmCallback(this);
         rabbitTemplate.setReturnsCallback(this);
-        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        rabbitTemplate.setMessageConverter(new JacksonJsonMessageConverter());
     }
 
 
