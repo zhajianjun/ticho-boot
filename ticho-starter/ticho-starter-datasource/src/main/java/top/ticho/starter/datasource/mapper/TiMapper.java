bd936fe95f78605744f0768f29fba90a3c58acb1
zhajianjun <1019319473@qq.com>
2025-12-16 23:10:59 +0800

    feat[ticho-starter-datasource]: 添加分页查询功能
    [- 新增 selectPage 方法支持分页查询
    - 支持通过 TiPageQuery 参数进行分页配置
    - 集成 MyBatis-Plus 分页插件实现分页逻辑
    - 提供带数据转换的分页查询重载方法
    - 引入 TiPageUtil 工具类处理分页结果转换
    - 添加相关导入依赖以支持新功能实现]

--- ticho-starter/ticho-starter-datasource/src/main/java/top/ticho/starter/datasource/mapper/TiMapper.java
+++ ticho-starter/ticho-starter-datasource/src/main/java/top/ticho/starter/datasource/mapper/TiMapper.java
@@ -1,9 +1,15 @@
 package top.ticho.starter.datasource.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
+import top.ticho.starter.datasource.util.TiPageUtil;
+import top.ticho.starter.view.core.TiPageQuery;
+import top.ticho.starter.view.core.TiPageResult;
 
 import java.util.Collection;
+import java.util.function.Function;
 
 /**
  * TiMapper接口，继承自MyBatis-Plus的BaseMapper，提供自定义的批量操作方法
@@ -44,5 +50,30 @@ public interface TiMapper<T> extends BaseMapper<T> {
      * @return 插入或更新操作影响的行数
      */
     int insertOrUpdateBatch(@Param("list") Collection<T> list);
+
+    /**
+     * 分页查询
+     *
+     * @param query        分页参数
+     * @param queryWrapper 查询条件
+     */
+    default <M extends TiPageQuery> TiPageResult<T> selectPage(M query, Wrapper<T> queryWrapper) {
+        Page<T> page = new Page<>(query.getPageNum(), query.getPageSize(), query.getCount());
+        selectPage(page, queryWrapper);
+        return TiPageUtil.of(page);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param query        分页参数
+     * @param queryWrapper 查询条件
+     * @param mapping      转换
+     */
+    default <M extends TiPageQuery, N> TiPageResult<N> selectPage(M query, Wrapper<T> queryWrapper, Function<T, N> mapping) {
+        Page<T> page = new Page<>(query.getPageNum(), query.getPageSize(), query.getCount());
+        selectPage(page, queryWrapper);
+        return TiPageUtil.of(page, mapping);
+    }
 
 }
