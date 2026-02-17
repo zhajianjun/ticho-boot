577b00c72b890858d70a6ab073ea6c478b1b30b0
zhajianjun <1019319473@qq.com>
2026-02-17 13:38:11 +0800

    feature[ticho-tool-ffmpeg]: 添加FFmpegProperty

---
+++ ticho-tool/ticho-tool-ffmpeg/src/main/java/top/ticho/tool/ffmpeg/FFmpegProperty.java
@@ -0,0 +1,23 @@
+package top.ticho.tool.ffmpeg;
+
+import lombok.Data;
+
+/**
+ * FFmpeg配置
+ *
+ * @author zhajianjun
+ * @date 2026-02-25 19:51
+ */
+@Data
+public class FFmpegProperty {
+
+    /** FFmpeg路径 */
+    private String path;
+    /** 超时时间(秒) */
+    private Long timeout;
+    /** 默认线程数 */
+    private Integer threads;
+    /** 硬件加速类型(cuda/vulkan) */
+    private String hardware;
+
+}
