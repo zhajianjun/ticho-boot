996a4c0880fb11da659cb79a1c3a3fc347cc4f3e
zhajianjun <1019319473@qq.com>
2026-03-04 22:04:10 +0800

    feat[ticho-tool-core]: 添加URL工具类文档注释

--- ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/TiUrlUtil.java
+++ ticho-tool/ticho-tool-core/src/main/java/top/ticho/tool/core/TiUrlUtil.java
@@ -9,13 +9,21 @@ import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 /**
- *
+ * URL 工具类
+ * <p>提供 URI 转换和 URL 编码等常用操作</p>
  *
  * @author zhajianjun
  * @date 2025-08-10 10:48
  */
 public class TiUrlUtil {
 
+    /**
+     * 将字符串转换为 URI 对象
+     *
+     * @param location 待转换的字符串，会自动去除首尾空白字符
+     * @return 转换后的 URI 对象
+     * @throws TiUtilException 当 URI 格式不正确时抛出此异常
+     */
     public static URI toURI(String location) {
         try {
             return new URI(TiStrUtil.trim(location));
@@ -24,6 +32,12 @@ public class TiUrlUtil {
         }
     }
 
+    /**
+     * 对 URL 进行 UTF-8 编码
+     *
+     * @param url 待编码的 URL 字符串
+     * @return 编码后的字符串，如果输入为 null 则返回 null
+     */
     public static String encode(String url) {
         if (Objects.isNull(url)) {
             return null;
